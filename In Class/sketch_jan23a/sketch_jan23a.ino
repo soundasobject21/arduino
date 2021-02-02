beb626a5d5b42e763cd648e7fdc40b0869b2a352
stephiescastle <stephiesmith@gmail.com>
2021-02-01 20:06:04 -0800

    oops, copypaste typo

--- In Class/sketch_jan23a/sketch_jan23a.ino
+++ In Class/sketch_jan23a/sketch_jan23a.ino
@@ -18,12 +18,4 @@ void loop() {
   digitalWrite(ledPin, LOW);
   delay(500);
 
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
