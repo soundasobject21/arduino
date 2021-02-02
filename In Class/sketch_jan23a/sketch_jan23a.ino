a1b9c285ebe2bae7a32af66dff4afe9663172d2a
stephiescastle <stephiesmith@gmail.com>
2021-02-01 20:04:09 -0800

    adding code that we completed together in class

---
+++ In Class/sketch_jan23a/sketch_jan23a.ino
@@ -0,0 +1,29 @@
+
+// declare and initialize variables outside of functions
+
+int ledPin = 13;
+
+void setup() {
+  // setup function
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // looping function
+  // put your main code here, to run repeatedly
+
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+
+}void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
