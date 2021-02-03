0824c56a913355487f5bf1d28182e42206241358
stephiescastle <stephiesmith@gmail.com>
2021-02-02 20:54:08 -0800

    updating code done together in class

--- In Class/sketch_jan26a/sketch_jan26a.ino
+++ In Class/sketch_jan26a/sketch_jan26a.ino
@@ -1,21 +1,37 @@
 
 // declare and initialize variables outside of functions
 
-int ledPin = 13;
+int ledPin = 13; // digital pin
+
+int knobPin = A0; // analog pin
+int knobValue = 0; // where to store knob value
 
 void setup() {
   // setup function
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
+
+  // start serial communications
+  Serial.begin(9600);
 }
 
 void loop() {
   // looping function
   // put your main code here, to run repeatedly
+  knobValue = analogRead(knobPin);
+  Serial.println(knobValue);
 
-  digitalWrite(ledPin, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  delay(500);
+  if (knobValue < 500) {
+    // if true, do this
+    digitalWrite(ledPin, HIGH);
+  } else if (knobValue > 900) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    // if not true, do this
+    digitalWrite(ledPin, HIGH);
+    delay(knobValue);
+    digitalWrite(ledPin, LOW);
+    delay(knobValue);
+  }
 
 }
